a8ae41b81f506055e0b2ed0307dee1e464b30757
Jin Han Park <wakeup5@outlook.kr>
2015-05-12 07:46:52 +0000

    Imported from Changeset 162

---
+++ WinAPI basic/stdafx.h
@@ -0,0 +1,59 @@
+/**
+@file	stdafx.h
+@author	박진한(wakeup5, wlsgks5@naver.com)
+@date	2015/05/08
+@brief	헤더 포함 리스트
+*/
+
+#pragma once
+
+#include "targetver.h"
+
+//MFC가 아닌 응용프로그램의 경우에는 WIN32_LEAN_AND_MEAN를 정의해 빌드 시간을 단축시킨다.
+#define WIN32_LEAN_AND_MEAN
+
+/* ========================================
+*  ## 헤더파일 ##
+* ======================================== */
+#include <Windows.h>	//윈도우 관련 헤더
+#include <stdio.h>		//표준 입출력 헤더
+#include <tchar.h>		//윈도우에서 사용할 문자열 출력 헤더
+//MBCS(Multi Byte Character Set) 기존의 사용하던 멀티바이트 형태의 문자열
+//WBCS(Widw Byte Character Set) 모든 문자를 2바이트로 처리
+
+#include <math.h>
+#include <string>
+#include <vector>
+
+//매크로 헤더
+#include "commonMacroFunction.h"
+
+/* ========================================
+*  ## 정의문 ##
+* ======================================== */
+
+//윈도우 이름
+#define WIN_NAME		(LPTSTR)(TEXT("API Window"))
+
+//윈도우 위치 사이즈
+#define WIN_START_X		100		//x위치
+#define WIN_START_Y		100		//y위치
+#define WIN_SIZE_X		500		//폭
+#define WIN_SIZE_Y		500		//높이
+
+//윈도우 스타일
+#define WIN_STYLE		WS_CAPTION | WS_SYSMENU
+
+//안전 동적할당 해제 매크로
+#define SAFE_DELETE(p)			{if (p) {delete(p); (p)=NULL;}}
+#define SAFE_DELETE_ARRAY(p)	{if (p) {delete[](p); (p)=NULL;}}
+#define SAFE_RELEASE(p)			{if (p) {(p)->release(); (p) = NULL;}}
+
+
+/* ========================================
+*  ## 전역변수 ##
+* ======================================== */
+extern HINSTANCE _hInstance; // 프로그램 인스턴스
+extern HWND _hWnd; // 윈도우 핸들
+
+using namespace std;
