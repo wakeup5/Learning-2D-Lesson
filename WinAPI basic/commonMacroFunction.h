e7418461d4388536657a587c52bbc755cc29e12a
Jin Han Park <wakeup5@outlook.kr>
2015-05-12 08:41:01 +0000

    Imported from Changeset 165

---
+++ WinAPI basic/commonMacroFunction.h
@@ -0,0 +1,153 @@
+/** commonMacroFunction
+@file	commonMacroFunction.h
+@author	박진한(wlsgks5@naver.com)
+@date	2015/05/12
+@brief	간편하게 사용할 인라인 매크로 함수들 선언.
+*/
+#include "stdafx.h"
+
+/** makePoint 좌표를 입력하면 포인트 변수를 만들어 리턴
+@author	박진한(wlsgks5@naver.com)
+@date	2015/05/12
+@param	x x좌표
+@param	y y좌표
+@return	POINT
+*/
+inline POINT makePoint(int x, int y)
+{
+	return POINT{ x, y };
+}
+
+/** makeRect 좌표와 폭을 입력하면 Rect를 만들어 리턴
+@author	박진한(wlsgks5@naver.com)
+@date	2015/05/12
+@param	int x left
+@param	int y top
+@param	int width 폭
+@param	int height 높이
+@return	RECT
+*/
+inline RECT makeRect(int x, int y, int width, int height)
+{
+	return RECT{ x, y, x + width, y + height };
+}
+
+/** makeRectCenter 중심 좌표를 중심으로 Rect를 만들어 리턴
+@author	박진한(wlsgks5@naver.com)
+@date	2015/05/12
+@param	int centerX 중심 x좌표
+@param	int centerY 중심 y좌표
+@param	int width 폭
+@param	int height 높이
+@return	RECT
+*/
+inline RECT makeRectCenter(int centerX, int centerY, int width, int height)
+{
+	return RECT{ centerX - (width / 2), centerY - (height / 2), centerX + (width / 2), centerY + (height / 2) };
+}
+
+/** drawLine 좌표를 입력받아 선을 그린다.
+@author	박진한(wlsgks5@naver.com)
+@date	2015/05/12
+@param	HDC 윈도우 dc의 핸들
+@param	int x1 시작 x좌표
+@param	int y1 시작 y좌표
+@param	int x2 끝 x좌표
+@param	int y2 끝 y좌표
+*/
+inline void drawLine(HDC hdc, int x1, int y1, int x2, int y2)
+{
+	MoveToEx(hdc, x1, y1, NULL);
+	LineTo(hdc, x2, y2);
+}
+
+/** drawLines 여러 좌표들을 받아 여러개의 선을 그린다.
+@author	박진한(wlsgks5@naver.com)
+@date	2015/05/12
+@param	HDC 윈도우 dc의 핸들
+@param	vector<POINT> 여러개의 포인터들
+*/
+inline void drawLines(HDC hdc, std::vector<POINT> pointVector)
+{
+	std::vector<POINT>::iterator iter;
+
+	for (iter = pointVector.begin(); iter != pointVector.end(); iter++)
+	{
+		if (iter == pointVector.begin()) MoveToEx(hdc, iter->x, iter->y, NULL);
+		else LineTo(hdc, iter->x, iter->y);
+	}
+}
+
+/** drawRectangle 좌표와 크기를 입력받아 사각형을 그린다.
+@author	박진한(wlsgks5@naver.com)
+@date	2015/05/12
+@param	HDC 윈도우 dc의 핸들
+@param	int x left
+@param	int y top
+@param	int width 폭
+@param	int height 높이
+*/
+inline void drawRectangle(HDC hdc, int x, int y, int width, int height)
+{
+	Rectangle(hdc, x, y, x + width, y + height);
+}
+
+/** drawRectangleCenter 좌표를 중심으로 사각형을 그린다.
+@author	박진한(wlsgks5@naver.com)
+@date	2015/05/12
+@param	HDC 윈도우 dc의 핸들
+@param	int centerX 중심 좌표 x
+@param	int centerY 중심 좌표 y
+@param	int width 폭
+@param	int height 높이
+*/
+inline void drawRectangleCenter(HDC hdc, int centerX, int centerY, int width, int height)
+{
+	Rectangle(hdc, centerX - (width / 2), centerY - (height / 2), centerX + (width / 2), centerY + (height / 2));
+}
+
+/** drawEllipse 좌표와 크기를 입력받아 원을 그린다.
+@author	박진한(wlsgks5@naver.com)
+@date	2015/05/12
+@param	HDC 윈도우 dc의 핸들
+@param	int x left
+@param	int y top
+@param	int width 폭
+@param	int height 높이
+*/
+inline void drawEllipse(HDC hdc, int x, int y, int width, int height)
+{
+	Ellipse(hdc, x, y, x + width, y + height);
+}
+
+/** drawEllipseCenter 좌표를 중심으로 원을 그린다.
+@author	박진한(wlsgks5@naver.com)
+@date	2015/05/12
+@param	HDC 윈도우 dc의 핸들
+@param	int centerX 중심 좌표 x
+@param	int centerY 중심 좌표 y
+@param	int width 폭
+@param	int height 높이
+*/
+inline void drawEllipseCenter(HDC hdc, int centerX, int centerY, int width, int height)
+{
+	Ellipse(hdc, centerX - (width / 2), centerY - (height / 2), centerX + (width / 2), centerY + (height / 2));
+}
+
+/** checkInRect POINT가 RECT 내에 있는지 체크. 충돌 체크
+@author	박진한(wlsgks5@naver.com)
+@date	2015/05/12
+@param	POINT
+@param	RECT
+@return	bool 안에 존재하는지의 여부
+*/
+inline bool checkInRect(POINT point, RECT rect)
+{
+	if (rect.left < point.x &&
+		rect.right >= point.x && 
+		rect.top < point.y && 
+		rect.bottom >= point.y)
+		return true;
+	else
+		return false;
+}
---
+++ WinAPI basic/win32main.cpp
@@ -0,0 +1,144 @@
+/**
+Win API
+@author	박진한(wakeup5, wlsgks5@naver.com)
+@date	2015/05/08
+@brief	윈도우 api 기본
+*/
+#include "stdafx.h"
+
+HINSTANCE _hInstance; // 프로그램 인스턴스
+HWND _hWnd; // 윈도우 핸들
+
+LRESULT CALLBACK WndProc(HWND, UINT, WPARAM, LPARAM);
+void setWindowPositionAndSize(int x, int y, int width, int height);
+
+int APIENTRY WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpszCmdParam, int cmdShow)
+{
+	MSG message;
+	WNDCLASS wndClass;
+
+	_hInstance = hInstance;
+
+	wndClass.cbClsExtra		= 0;									// 클래스 여분 메모리
+	wndClass.cbWndExtra		= 0;									// 윈도우 여분 메모리
+	wndClass.hbrBackground	= (HBRUSH)GetStockObject(WHITE_BRUSH);	// 백그라운드
+	wndClass.hCursor		= LoadCursor(NULL, IDC_ARROW);			// 커서 모양
+	wndClass.hIcon			= LoadIcon(NULL, IDI_APPLICATION);		// 상위 창 아이콘 모양
+	wndClass.hInstance		= hInstance;							// 윈도우 인스턴스
+	wndClass.lpfnWndProc	= (WNDPROC)WndProc;						// (*중요)프로시저 함수(메세지 처리함수) - 키보드 입력등에 호출되는 함수
+	wndClass.lpszClassName	= WIN_NAME;					// 클래스 명(화면 창 이름)
+	wndClass.lpszMenuName	= NULL;									// 메뉴명
+	wndClass.style			= CS_HREDRAW | CS_VREDRAW;				// 윈도우 창 상단오른쪽 버튼 스타일
+
+	RegisterClass(&wndClass);
+
+	_hWnd = CreateWindow(
+		WIN_NAME,		// 윈도우 클래스 이름
+		WIN_NAME,		// 윈도우 타이틀바의 이름
+		WIN_STYLE,		// 윈도우 스타일
+		WIN_START_X,	// 윈도우 화면 좌표 x(좌우) - 픽셀 단위
+		WIN_START_Y,	// 윈도우 화면 좌표 y(상하) - 픽셀 단위
+		WIN_SIZE_X,		// 윈도우 화면 크기 width(폭)
+		WIN_SIZE_Y,		// 윈도우 화면 크기 height(높이)
+		NULL,			// 부모 윈도우 인스턴스
+		(HMENU)NULL,	// 메뉴 핸들
+		hInstance,		// 인스턴스 윈도우 지정
+		NULL			// mdi 클라이언트 윈도우 및 자식 윈도우를 생성하면 지정해 주고, 사용하지 않으면 NULL
+		);
+
+	setWindowPositionAndSize(WIN_START_X, WIN_START_Y, WIN_SIZE_X, WIN_SIZE_Y);
+
+	ShowWindow(_hWnd, cmdShow);
+
+	//게임용
+	/*
+	while (true)
+	{
+	if (PeekMessage(&message, NULL, 0, 0, PM_REMOVE))
+	{
+	if (message.message == WM_QUIT)
+	{
+	break;
+	}
+	TranslateMessage(&message);
+	DispatchMessage(&message);
+	}
+	//처리할 메세지가 없으면 렌더 호출
+	//render();
+	}
+	*/
+
+	//일반 윈도우 창
+	while (GetMessage(&message, 0, 0, 0))
+	{
+		TranslateMessage(&message);
+		DispatchMessage(&message);
+	}
+
+	UnregisterClass(WIN_NAME, _hInstance);
+
+	return message.wParam;
+}
+
+LRESULT CALLBACK WndProc(HWND hWnd, UINT iMessage, WPARAM wParam, LPARAM lParam)
+{
+	PAINTSTRUCT ps;
+	HDC hdc;
+
+	static RECT r1;
+
+	switch (iMessage)
+	{
+	case WM_CREATE:
+	
+		break;
+	case WM_PAINT:
+		hdc = BeginPaint(hWnd, &ps);
+		
+		r1 = makeRectCenter(250, 250, 200, 200);
+		Rectangle(hdc, r1.left, r1.top, r1.right, r1.bottom);
+
+		EndPaint(hWnd, &ps);
+		break;
+	case WM_MOUSEMOVE:
+	{
+
+		hdc = GetDC(hWnd);
+
+		int mx = LOWORD(lParam);
+		int my = HIWORD(lParam);
+
+		char p[128];
+
+		if (checkInRect(makePoint(mx, my), r1))
+		{
+			wsprintf(p, "%4d, %4d", mx, my);
+			TextOut(hdc, 10, 10, p, _tcslen(p));
+		}
+		else
+		{
+			InvalidateRect(hWnd, NULL, true);
+		}
+
+		ReleaseDC(hWnd, hdc);
+	}
+		break;
+	case WM_DESTROY: //프로그램 종료시 발생.
+		PostQuitMessage(0);
+		return 0;
+	}
+
+	return DefWindowProc(hWnd, iMessage, wParam, lParam);
+}
+
+void setWindowPositionAndSize(int x, int y, int width, int height)
+{
+	RECT rc;
+	rc.left = 0;
+	rc.top = 0;
+	rc.right = width;
+	rc.bottom = height;
+
+	AdjustWindowRect(&rc, WS_OVERLAPPEDWINDOW, false);
+	SetWindowPos(_hWnd, NULL, x, y, rc.right - rc.left, rc.bottom - rc.top, SWP_NOZORDER | SWP_NOMOVE);
+}
