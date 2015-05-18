1523e197890b06c8961702349e1e234f9ac845a5
Jin Han Park <wakeup5@outlook.kr>
2015-05-18 09:10:37 +0000

    Imported from Changeset 211

--- WinAPI basic/GameStudy.cpp
+++ WinAPI basic/GameStudy.cpp
@@ -6,6 +6,9 @@ HRESULT GameStudy::initialize(void)
 {
 	GameNode::initialize();
 
+	_center.x = WIN_SIZE_X / 2;
+	_center.y = WIN_SIZE_Y / 2;
+
 	return S_OK;
 }
 
@@ -19,10 +22,54 @@ void GameStudy::release(void)
 void GameStudy::update(void)
 {
 	GameNode::update();
+
+	timer = time(NULL);
+	localtime_s(&t, &timer);
+
+	_hour = t.tm_hour;
+	_minite = t.tm_min;
+	_second = t.tm_sec;
+
+	//초침 끝자리 좌표
+	double secondRadian = ((_second / 60 * 360) - 90) * M_PI / 180;
+	_sp.x = _center.x + LENGTH_SECOND * cos(secondRadian);
+	_sp.y = _center.y + LENGTH_SECOND * sin(secondRadian);
+
+	//분침 끝자리 좌표
+	double miniteRadian = (((_minite / 60 * 360) - 90) * M_PI / 180) + secondRadian / 60;
+	_mp.x = _center.x + LENGTH_MINITE * cos(miniteRadian);
+	_mp.y = _center.y + LENGTH_MINITE * sin(miniteRadian);
+
+	//시침 끝자리 좌표
+	double hourRadian = (((_hour / 12 * 360) - 90) * M_PI / 180) + miniteRadian / 60;
+	_hp.x = _center.x + LENGTH_HOUR * cos(hourRadian);
+	_hp.y = _center.y + LENGTH_HOUR * sin(hourRadian);
+
 }
 
 //화면출력
 void GameStudy::render(HDC hdc)
 {
+	//시계 가장자리 출력
+	drawEllipseCenter(hdc, _center, WATCH_SIZE, WATCH_SIZE);
+	drawEllipseCenter(hdc, _center, 5, 5);
 
+	HPEN p;
+
+	//시침
+	p = (HPEN)SelectObject(hdc, CreatePen(PS_SOLID, 5, RGB(0, 0, 0)));
+	drawLine(hdc, _center.x, _center.y, _hp.x, _hp.y);
+	//분침
+	p = (HPEN)SelectObject(hdc, CreatePen(PS_SOLID, 3, RGB(0, 0, 0)));
+	drawLine(hdc, _center.x, _center.y, _mp.x, _mp.y);
+	//초침
+	p = (HPEN)SelectObject(hdc, CreatePen(PS_SOLID, 1, RGB(255, 0, 0)));
+	drawLine(hdc, _center.x, _center.y, _sp.x, _sp.y);
+	//SelectObject(hdc, p);
+	//DeleteObject(p);
+
+
+	TCHAR time[128];
+	sprintf_s(time, "%0.0f : %0.0f : %0.0f", _hour, _minite, _second);
+	TextOut(hdc, 10, 10, time, _tcslen(time));
 }
