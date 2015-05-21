ad108a7e21e835925ad522c9971492f1d6016a85
Jin Han Park <wakeup5@outlook.kr>
2015-05-21 02:31:29 +0000

    Imported from Changeset 229

--- WinAPI basic/GameStudy.h
+++ WinAPI basic/GameStudy.h
@@ -1,13 +1,6 @@
 #pragma once
-#define MAX_BULLET 10
-#define MAX_POWER 20
-#define GRAVITY_ACCEL 0.2
-
-#define RADER_RANGE 500
 
 #include "GameNode.h"
-#include "Cannon.h"
-#include "Bullet.h"
 
 using namespace std;
 
@@ -15,21 +8,7 @@ class GameStudy :
 	public GameNode
 {
 private:
-    Cannon _cannon;
-    Bullet _bullet[MAX_BULLET];
-    float _bulletEnd[MAX_BULLET];
-
-    Cannon _enemy;
-
-    float _power;
-    float _shootPower;
-
-    float _firstSpeedY;
-
-    float _enemyAngle = 110;
-    float _enemyPower;
-    Bullet _enemyBullet;
-
+    
 public:
 	virtual HRESULT initialize(void);
 	virtual void release(void);
