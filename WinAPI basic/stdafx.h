fb86b41a487c145e5dad7be587027498837e2533
Jin Han Park <wakeup5@outlook.kr>
2015-05-29 06:05:26 +0000

    Imported from Changeset 265

--- WinAPI basic/stdafx.h
+++ WinAPI basic/stdafx.h
@@ -49,8 +49,8 @@
 //윈도우 위치 사이즈
 #define WIN_START_X		100		//x위치
 #define WIN_START_Y		100		//y위치
-#define WIN_SIZE_X		1000		//폭
-#define WIN_SIZE_Y		1000		//높이
+#define WIN_SIZE_X		500		//폭
+#define WIN_SIZE_Y		500		//높이
 
 //윈도우 스타일
 #define WIN_STYLE		WS_CAPTION | WS_SYSMENU
