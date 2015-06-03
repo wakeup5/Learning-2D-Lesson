448259287351689341dff6e3190d24b89a942c58
Jin Han Park <wakeup5@outlook.kr>
2015-06-03 09:07:44 +0000

    Imported from Changeset 283

--- WinAPI basic/ImageManager.cpp
+++ WinAPI basic/ImageManager.cpp
@@ -87,52 +87,32 @@ Image* ImageManager::addImage(std::string key, const char* fileName, float cente
 	return image;
 }
 
-/*
 SpriteImage* ImageManager::addSpriteImage(std::string key, const char* fileName, int width, int height, int frameColumn, int frameRow, BOOL trans, COLORREF transColor)
 {
-	SpriteImage *image = static_cast<SpriteImage*>(findImage(key));
-
-	if (image != NULL)
+	Image* image = addImage(key, fileName, width, height, trans, transColor);
+	SpriteImage* spriteImage;
+	if (image == NULL)
 	{
-		return image;
-	}
-
-	image = new SpriteImage();
-
-	if (FAILED(image->initialize(fileName, width, height, frameColumn, frameRow, trans, transColor)))
-	{
-		SAFE_DELETE(image);
 		return NULL;
 	}
 
-	_imageMap.insert(std::make_pair(key, image));
-
-	return image;
+	spriteImage = image->getSpriteImage(frameColumn, frameRow);
+	return spriteImage;
 }
-*/
-/*
+
 SpriteImage* ImageManager::addSpriteImage(std::string key, const char* fileName, float centerX, float centerY, int width, int height, int frameColumn, int frameRow, BOOL trans, COLORREF transColor)
 {
-	SpriteImage *image = static_cast<SpriteImage*>(findImage(key));
-
-	if (image != NULL)
+	Image* image = addImage(key, fileName, centerX, centerY, width, height, trans, transColor);
+	SpriteImage* spriteImage;
+	if (image == NULL)
 	{
-		return image;
-	}
-
-	image = new SpriteImage();
-
-	if (FAILED(image->initialize(fileName, centerX, centerY, width, height, frameColumn, frameRow, trans, transColor)))
-	{
-		SAFE_DELETE(image);
 		return NULL;
 	}
 
-	_imageMap.insert(std::make_pair(key, image));
-
-	return image;
+	spriteImage = image->getSpriteImage(frameColumn, frameRow);
+	return spriteImage;
 }
-*/
+
 Image* ImageManager::findImage(std::string key)
 {
 	ImageMapIter imageIter = _imageMap.find(key);
